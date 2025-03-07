98e27ecd74b1520e250c105dc024a3cf86f83e22
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-03-07 06:43:31 +0100

    first commit

---
+++ arrays.js
@@ -0,0 +1,8 @@
+// Write a program that returns the last item in an array
+const array = [1, 2, 3, 4, 5];
+
+function getLastItem(array) {
+  return array.at(-1);
+}
+
+console.log(getLastItem([1, 2, 3, 4, 5]));
